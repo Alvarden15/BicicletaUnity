9e084f9bfa401ebc552691b2b1f53d6e4edb914b
Alvarden <alvaroracchumi@hotmail.com>
2020-09-21 14:14:50 -0500

    Primer commit
    Este es el codigo fuente del asset. De ahi es donde viene lo de la bicicleta (assets extra: cinemachine, Lean Pool).

---
+++ Bicicleta/Assets/bicycle_PRO_kit/Scripts/toMainMenu.cs
@@ -0,0 +1,20 @@
+﻿// Writen by Boris Chuprin smokerr@mail.ru
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class toMainMenu : MonoBehaviour {
+
+	// Es redundante ahora. Eliminen esto.
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if  (Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene("BPK_MainMenu");
+			
+		}
+	}
+}
